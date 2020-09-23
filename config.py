0dbe786f2582285528d9699a772755511b006ca7
PerisOduol618 <oduolpepe618@gmail.com>
2020-09-23 16:52:56 +0300

    Add emails section

---
+++ config.py
@@ -0,0 +1,52 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = 'penyo'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/pitch'
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/pitch'
+
+    DEBUG = True
+    ENV = 'development'
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY= 'penyo'
+export MAIL_USERNAME='oduolpepe618@gmail.com'
+export MAIL_PASSWORD='37959713Sam'
+
+python manage.py server
