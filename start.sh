70a6507642b578eb58138eec10e16450b2b45a5c
PerisOduol618 <oduolpepe618@gmail.com>
2020-09-24 21:43:18 +0300

    Test the application

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY= 'penyo'
-export MAIL_USERNAME='oduolpepe618@gmail.com'
-export MAIL_PASSWORD='37959713Sam'
+# export MAIL_USERNAME='oduolpepe618@gmail.com'
+# export MAIL_PASSWORD='37959713Sam'
 
 python manage.py server
