cbb5b0b78816621c7090e6bf5bb10a8ef39a40b1
PerisOduol618 <oduolpepe618@gmail.com>
2020-09-24 21:40:02 +0300

    remove the mail section

--- app/auth/views.py
+++ app/auth/views.py
@@ -28,7 +28,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to the Best Pitch","email/welcome_user",user.email,user=user)
+        # mail_message("Welcome to the Best Pitch","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
